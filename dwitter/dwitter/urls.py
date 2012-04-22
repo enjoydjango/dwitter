369e9089687856d8d9a49ff601bc06e7d7b25eaf
Jaime Irurzun <jaime.irurzun@gmail.com>
2012-04-22 18:25:50 +0100

    Implement individual page for a tweet

--- dwitter/dwitter/urls.py
+++ dwitter/dwitter/urls.py
@@ -14,5 +14,6 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     # url(r'^admin/', include(admin.site.urls)),
+    url(r'^user/\w+/status/(\d+)/$', 'website.views.tweet_page'),
     url(r'^$', 'website.views.timeline'),
 )
--- dwitter/website/views.py
+++ dwitter/website/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 from website.models import Tweet
 
@@ -6,3 +6,8 @@ from website.models import Tweet
 def timeline(request):
     tweets = Tweet.objects.all()
     return render(request, 'website/index.html', {'tweets': tweets})
+
+
+def tweet_page(request, tweet_id):
+    tweet = get_object_or_404(Tweet, id=tweet_id)
+    return render(request, 'website/tweet_page.html', {'tweet': tweet})
