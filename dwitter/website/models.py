790f873999d7c29aa5ce5219d14cfa1b0715e098
Jaime Irurzun <jaime.irurzun@gmail.com>
2012-04-22 17:43:26 +0100

    Create Tweet model

--- dwitter/website/models.py
+++ dwitter/website/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Tweet(models.Model):
+    message = models.TextField(blank=True, max_length=140)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(User)
+
+    def __unicode__(self):
+        return self.message
