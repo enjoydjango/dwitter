aad9c83ff0f587786cb5b8b972d4eee767351f17
Jaime Irurzun <jaime.irurzun@gmail.com>
2012-04-29 16:49:05 +0100

    Create mention custom filter

---
+++ dwitter/website/templatetags/dwitter.py
@@ -0,0 +1,19 @@
+import re
+
+from django import template
+from django.template.defaultfilters import stringfilter
+
+
+register = template.Library()
+
+
+@register.filter(name='mention')
+@stringfilter
+def mention(value):
+    return re.sub(r'\@(\w+)', r'<a href="/user/\1/">@\1</a>', value)
+    # DRY solution using reverse():
+    # from django.core.urlresolvers import reverse
+    # return re.sub(r'\@(\w+)', lambda u: r'<a href="%s">@%s</a>' %
+    #     (reverse('user-page', args=u.groups()), u.groups()[0]), value)
+
+mention.is_safe = True
