8ac451d0904ecd4ae8d944551b3285c31c5e166c
Jaime Irurzun <jaime.irurzun@gmail.com>
2012-04-29 14:19:58 +0100

    Add form for tweeting

--- dwitter/website/views.py
+++ dwitter/website/views.py
@@ -1,12 +1,24 @@
 from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponseRedirect
 from django.contrib.auth.models import User
 
 from website.models import Tweet
+from website.forms import TweetForm
 
 
 def timeline(request):
+    if request.method == "POST":
+        form = TweetForm(request.POST)
+        if form.is_valid():
+            t = Tweet()
+            t.user = request.user
+            t.message = form.cleaned_data['message']
+            t.save()
+            return HttpResponseRedirect("/")
+    else:
+        form = TweetForm()
     tweets = Tweet.objects.all().order_by('-timestamp')
-    return render(request, 'website/index.html', {'tweets': tweets})
+    return render(request, 'website/index.html', {'tweets': tweets, 'form': form})
 
 
 def tweet_page(request, tweet_id):
